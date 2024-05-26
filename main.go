7f385eb8d5fe160221e0ea64fc0a63b5b616ad92
to <tuvotg123@gmail.com>
2024-05-26 18:40:08 -0400

    better saving

--- main.go
+++ main.go
@@ -20,3 +20,20 @@ func saveDataNaive(path string, data []byte) error {
 	return err
 
 }
+
+func saveDataBetter(path string, data []byte) error {
+	tmp := fmt.Sprintf("%s.tmp.%d", path, randomInt())
+	fp, err := os.OpenFile(tmp, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0604)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+
+	_, err = fp.Write(data)
+	if err != nil {
+		os.Remove(tmp)
+		return err
+	}
+
+	return os.Rename(tmp, path)
+}
